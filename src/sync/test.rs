e4a40996f43a1e98f1361276cd782869d8e18219
Nathan Blinn <nathan.blinn@mongodb.com>
2021-07-29 16:54:34 -0400

    RUST-734 Document support for sharded transactions

--- src/sync/test.rs
+++ src/sync/test.rs
@@ -202,8 +202,8 @@ fn transactions() {
 
     let should_skip = RUNTIME.block_on(async {
         let test_client = AsyncTestClient::new().await;
-        // TODO RUST-734: Unskip this test on sharded clusters when transactions are complete.
-        !test_client.is_replica_set() || test_client.server_version_lt(4, 0)
+        !(test_client.is_replica_set() && test_client.server_version_gte(4, 0)
+            || test_client.is_sharded() && test_client.server_version_gte(4, 2))
     });
     if should_skip {
         return;
